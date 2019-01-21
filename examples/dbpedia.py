a94912d98040a79a59aebce51d7eff09af1a1194
aishahasmoh <aishahasmoh@gmail.com>
2019-01-21 10:12:45 +0300

    add Pathlib to requirements

--- examples/dbpedia.py
+++ examples/dbpedia.py
@@ -3,16 +3,16 @@ from rdf2srl.rdfloader import RDFGraphDataset
 if __name__ == "__main__":
     loader = RDFGraphDataset(sparql_endpoint="http://dbpedia.org/sparql/")
     m = loader.num_triples() # 2,534,046
-	n_e = loader.num_entities() # 
-	n_p = loader.num_predicates() # 60,649
-	n_r = loader.num_relations() # 
-	n_a = loader.num_attributes() # 
-	n_al = loader.num_attr_literal_pairs() #
+    n_e = loader.num_entities() #
+    n_p = loader.num_predicates() # 60,649
+    n_r = loader.num_relations() #
+    n_a = loader.num_attributes() #
+    n_al = loader.num_attr_literal_pairs() #
 
     print("n_e = {}  n_p = {}  n_r = {}  n_a = {}  n_al = {}  m = {}".format(n_e, n_p, n_r, n_a, n_al, m))
 
-	e2l_m = loader.num_entity2literal_triples() # 
-	e2e_m = loader.num_entity2entity_triples()
+    e2l_m = loader.num_entity2literal_triples() #
+    e2e_m = loader.num_entity2entity_triples()
 	num_rdf_type_triples = loader.num_rdf_type_triples()
 	print("num_entity2entity_triples = {}".format(e2e_m)) 
 	print("num_entity2literal_triples = {}".format(e2l_m)) # 
